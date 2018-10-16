9111b1e93143f6e24806d908486999011fd1809e
Quikes <quikeskun@gmail.com>
2018-10-16 23:20:01 +0200

    basic door opening, some ugly quick wall+door made in blender + some higher-quality donuts i made

---
+++ Survival game/Scripts only/MyOwnScripts/Crouch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour {
+
+
+    private float minimum;
+    private float maximum;
+    public CharacterController controller;
+	// Use this for initialization
+	void Start () {
+        minimum = 1.2f;
+        maximum = 1.8f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.C) && controller.height>1.2f){
+            controller.height -= 5f * Time.deltaTime;
+        }
+        else if(controller.height<1.8f && !Input.GetKey(KeyCode.C))
+        {
+            controller.height += 5f * Time.deltaTime;
+            gameObject.transform.position += new Vector3(0, 0.3f,0);
+
+        }
+        
+
+
+    }
+    
+}
---
+++ Survival game/Scripts only/MyOwnScripts/DoorLogic.cs
@@ -0,0 +1,79 @@
+﻿
+using UnityEngine;
+
+public class DoorLogic : MonoBehaviour {
+
+    private bool drawGUI = false;
+    public GameObject door;
+    private Animator anim;
+    private bool doorOpen;
+    private string doorstate;
+    
+	// Use this for initialization
+	void Start () {
+
+        
+        anim = door.GetComponentInParent<Animator>();
+        anim.SetBool("DoorOpen", false);
+        doorOpen = false;
+        doorstate = "close";
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(drawGUI==true && Input.GetKeyDown(KeyCode.E))
+        {
+            ChangeDoorState();
+            if (doorOpen)
+            {
+                doorstate = "open";
+
+            }
+            else
+            {
+                doorstate = "close";
+            }
+            
+        }
+
+
+		
+	}
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Player")
+
+        {
+            
+            Debug.Log("Works");
+            drawGUI = true;
+        }
+        
+
+        
+    }
+    public void OnTriggerExit(Collider collider)
+    {
+        drawGUI = false;
+        
+    }
+    public void OnGUI()
+    {
+        if (drawGUI == true)
+        {
+
+        GUI.Box(new Rect(Screen.width*0.5f-75,Screen.height*0.2f-11,150,22), "Press 'E' to "+ doorstate);
+        }
+
+    }
+    public void ChangeDoorState()
+    {
+        doorOpen = !doorOpen;
+        anim.SetBool("DoorOpen", doorOpen);
+        
+
+    }
+}
